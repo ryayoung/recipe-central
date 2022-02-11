54730160f4d980943dc31663d961084c40a51131
Ryan Young <ryan.d.young@du.edu>
2022-02-11 23:54:28 +0000

    Completed lab 09

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -1,24 +1,19 @@
-// Maintainer:     Ryan Young
-// Last Modified:  Jan 18, 2022
 import express from 'express';
-import * as controller from './users.controller'
+import * as controller from './users.controller';
 
-// Declare an Express.js Router instance
 let router = express.Router();
 
 // GET methods
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 
-// POST methods
+// POST method
 router.post('/', controller.create);
 
 // PUT method
-router.put('/:id', controller.upsert);
+router.put('/:id', controller.update);
 
 // DELETE method
 router.delete('/:id', controller.destroy);
 
-// Export the Express.js Router for other files to use (sucha as /server/routes.js)
 export {router};
-
--- server/api/users/users.model.js
+++ server/api/users/users.model.js
@@ -1,60 +1,65 @@
-import { v4 as uuidv4 } from 'uuid';
+import mongoose from 'mongoose';
+let Schema = mongoose.Schema;
 
-class User {
-  users = [];
+/*
+  This section declares the schemas for the different documents
+  that will be used
+ */
 
-  find() {
-    return this.users;
-  }
+// This schema represents the address of the user
+let addressSchema = Schema({
+  // addressLine1 is a simple String type that is required
+  addressLine1: {type: String, required: true},
+  // addressLine2 is a simple String type that is NOT required
+  addressLine2: {type: String, required: false},
+  // city is a simple String type that is required
+  city: {type: String, required: true},
+  // state is a simple String type that is required
+  state: {type: String, required: true},
+  // zip is a simple Number type that is required
+  zip: {type: Number, required: true}
+});
 
-  findById(id) {
-    let foundUsers = this.users
-        .filter(user => user.id === id);
+// This schema represents the name of the user
+let nameSchema = Schema({
+  // firstName is a simple String type that is required
+  firstName: {type: String, required: true},
+  // middleName is a simple String type that is not required
+  middleName: {type: String, required: false},
+  // lastName is a simple String type that is required
+  lastName: {type: String, required: true}
+});
 
-    if (foundUsers.length > 0) {
-        return foundUsers[0];
-    } else {
-        return null;
-    }
-  }
+// This is the main user schema
+let userSchema = Schema({
+  // Age is a simple number type that is required
+  age: {type: Number, required: true},
+  /*
+   Address is referenced as a 'foreign key' using the objectId
+   of an address stored in a separate collection.
+   The address will be populated by Mongoose using 'Population'
+   http://mongoosejs.com/docs/populate.html
+  */
+  address: {type: Schema.Types.ObjectId, ref: 'Address'},
+  /*
+   Name is a subdocument of User, and will be stored
+   in the same document as the User itself.
+   Unlike a populated document, this doesn't require an
+   ObjectId reference and the schema for name can be
+   referenced directly
+  */
+  name: nameSchema
+});
 
+/*
+  This section creates interactive models from the defined schemas
+  above so that you can perform Create Read Update and Delete (CRUD)
+  operations against the schemas.
+  NOTE since the nameSchema is embedded within userSchema, it does NOT have
+  to be created as a model!
+ */
+let Address = mongoose.model('Address', addressSchema);
+let User = mongoose.model('User', userSchema);
 
-  create(user) {
-      let id = uuidv4();
-      user.id = id;
-      this.users.push(user);
-      return user;
-  }
-
-
-  findOneAndUpdate(user) {
-      let index = this._findUserIndex(user.id);
-      if (index >= 0) {
-          this.users[index] = user;
-          return true;
-      } else {
-          this.users.push(user);
-          return false;
-      }
-  }
-
-
-  remove(id) {
-      let index = this._findUserIndex(id);
-
-      if (index >= 0) {
-          this.users.splice(index, 1);
-          return true;
-      } else {
-          return false;
-      }
-  }
-
-  _findUserIndex(id) {
-      return this.users
-          .map(user => user.id)
-          .indexOf(id);
-  }
-}
-
-export default new User();
+// Export the two created models, Address and User
+export {Address, User};
